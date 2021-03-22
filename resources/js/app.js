5206cf3daa82e5873867998d4c3124673ec67741
Félix Dorn <github@felixdorn.fr>
2021-03-22 14:14:32 +0100

    prevent livewire from tracking web components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,29 @@
-window.Noty = require('noty');
 require('alpinejs')
+
+
+
+
+
+ !el.tagName.includes("-") && el.hasAttribute('wire:observe')/**
+* Prevents Livewire from tr
+*/
+
+const livewireShouldObserve= (el) => !el.tagName.includes("-") || el.hasAttribute('wire:observe') 
+
+window.livewire.hook('element.initialized', el => {
+   if (!livewireShouldObserve(el)) {
+       return;
+   }
+  
+   el.__livewire_ignore = true;
+})
+
+window.livewire.hook('element.updating', (fromEl, toEl, component) => {
+    if (!livewireShouldObserve(fromEl)) {
+        return;
+    }
+
+    for (var i = 0, atts = toEl.attributes, n = atts.length, arr = []; i < n; i++){
+        fromEl.setAttribute(atts[i].nodeName, atts[i].nodeValue);
+    }
+})
